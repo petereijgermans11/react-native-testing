63c4927dd64b8b87485fd0bc26051ac9d7f945ad
Peter Eijgermans <p.eijgermans11@gmail.com>
2022-11-25 16:22:18 +0100

    react-native-e2e-jest-appium-webdriverio

---
+++ Appium/react-native-e2e-jest-appium-webdriverio/__tests__/basicE2E.test.js
@@ -0,0 +1,49 @@
+const wdio = require('webdriverio');
+import platformConfig from '../e2e-config';
+
+describe('Appium with Jest automation testing', () => {
+  let client;
+
+  beforeAll(async function () {
+    const config = {
+      path: '/wd/hub',
+      port: 4723,
+      capabilities: {
+        ...platformConfig,
+      },
+    };
+
+    client = await wdio.remote(config);
+
+
+  });
+
+  afterAll(async function () {
+    await client.pause(1500);
+    console.info('[afterAll] Done with testing!');
+
+    await client.deleteSession();
+  });
+
+  test('First test', async function () {
+    const loginStatusText = await client.$('~login-status');
+    let loginStatusTextValue = await loginStatusText.getText();
+    expect(loginStatusTextValue).toBe('fail');
+
+    const UsernameTextInput = await client.$('~username-textinput');
+    await UsernameTextInput.setValue('Morgan Freeman');
+    const UsernameTextInputValue = await UsernameTextInput.getText();
+    expect(UsernameTextInputValue).toBe('Morgan Freeman');
+
+    const PasswordTextInput = await client.$('~password-textinput');
+    await PasswordTextInput.setValue('god');
+    const PasswordTextInputValue = await PasswordTextInput.getText();
+    expect(PasswordTextInputValue).toBe('•••');
+
+    const loginButton = await client.$('~login-button');
+    await loginButton.click();
+
+    loginStatusTextValue = await loginStatusText.getText();
+    expect(loginStatusTextValue).toBe('success');
+  });
+});
